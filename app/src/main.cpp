163fef419c85127116287b410e18ed3b86366f60
Hussein Taher <hussein_taher@outlook.com>
2020-02-13 22:28:52 +0100

    updated gl version

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -29,8 +29,8 @@ int main(void) {
 
 	if (!glfwInit()) exit(EXIT_FAILURE);
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 
 	window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
 	if (!window) {
